9989827e705b253b0690677a5772d596a712a6ba
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-17 19:16:04 +0530

    set logog

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import logo from "../assets/chef.jpg"
 const Header = () => {
-
-}
-export default Header
+  return (
+    <>
+    <header>
+      <div className="brand">
+        <img src={logo} alt="Chef  logo" className="brand__logo" />
+        <span className="brand__name">virtual Chef</span>
+      </div>
+    </header>
+    </>
+  );
+};
+export default Header;
