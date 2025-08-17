d82091875fdf9e4d24ba734c7ca41228cc91b8f1
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-17 19:15:41 +0530

    set logo

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 const Form = () => {
+    return (
+        <main>
+        <form className="ingrident-form ">
+            <input
+              type="text"
+            />
+            <button>Add ingrident</button>
+        </form>
+    </main>
+    )
 
 }
 export default Form
