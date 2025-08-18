3769ba63ecf7718acfe35fae7798d349a5e3470c
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-18 18:15:49 +0530

    fix issue

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 const Form = () => {
-  const [ingridents, setIngridents] = useState(false);
+  const [ingridents, setIngridents] = useState([]);
   const ingridentItems = ingridents.map((ingrident) => {
     return <li key={ingrident}>{ingrident}</li>;
   });
@@ -21,7 +21,7 @@ const Form = () => {
         <input type="text"  name="ingrident"/>
         <button>Add ingrident</button>
       </form>
-    {ingridents &&  <section>
+    {ingridents.length > 0 &&  <section>
         <h2>Ingrident on hand : </h2>
         <ul className="Ingrident-list">{ingridents}</ul>
         <div className="get-recipe-container">
